d40804dad59e6f97d467a1d53ca980abd8e3e83a
Mittesh <mitteshwaghule09@gmail.com>
2023-09-19 18:53:08 +0530

    Added Html, CSS and JS

---
+++ main.js
@@ -0,0 +1,23 @@
+// sample dress data
+const dressesData = [
+    { name: "Dress 1", price: "Rs. 799" },
+    { name: "Dress 2", price: "Rs. 899" },
+    { name: "Dress 3", price: "Rs. 999" },
+];
+
+// displaying dress items
+function displayDresses() {
+    const dressesSection = document.getElementById("dresses");
+
+    dressesData.forEach((dress) => {
+        const dressElement = document.createElement("div");
+        dressElement.classList.add("dress");
+        dressElement.innerHTML = `
+            <h3>${dress.name}</h3>
+            <p>Price: ${dress.price}</p>
+        `;
+        dressesSection.appendChild(dressElement);
+    });
+}
+
+displayDresses();
