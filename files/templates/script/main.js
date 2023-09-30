dca9e89fbfa503470338caf805cc9f8b9d0a4aa2
Mittesh <mitteshwaghule09@gmail.com>
2023-09-30 23:40:28 +0530

    modified html, css and js

--- files/templates/script/main.js
+++ files/templates/script/main.js
@@ -14,15 +14,43 @@ function displayDresses() {
         dressElement.classList.add("dress");
         dressElement.innerHTML = `
             <h3>${dress.name}</h3>
-            `;
             <p>Price: ${dress.price}</p>
+        `;
         dressesSection.appendChild(dressElement);
     });
 }
 
+// navigate through different pages like from home to dresses to contact, etc
 function navigateToPage(pageUrl) {
     // set the current pages url to the clicked page URL
     window.location.href = pageUrl;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector(".slider");
+    const imgWidth = document.querySelector(".img").offsetWidth;
+    let currentIndex = 0;
+
+    document.querySelector(".next").addEventListener("click", function() {
+        currentIndex++;
+        if (currentIndex >= slider.children.length) {
+            currentIndex = 0;
+        }
+        updateSlider();
+    });
+
+    document.querySelector(".prev").addEventListener("click", function() {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = slider.children.length - 1;
+        }
+        updateSlider();
+    });
+
+    function updateSlider() {
+        const offset = -1 * currentIndex * imgWidth;
+        slider.style.transform = `translateX(${offset}px)`;
+    }
+})
 
 displayDresses();
